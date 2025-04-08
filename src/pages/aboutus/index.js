c622f2160f7333b733f9312af25dc0e26b58cdcb
MahaGit1233 <maharush5409@gmail.com>
2025-04-08 19:16:54 +0530

    link commit

--- src/pages/aboutus/index.js
+++ src/pages/aboutus/index.js
@@ -1,5 +1,22 @@
+import Link from "next/link";
+
 function aboutUs() {
-  return <h1>This page tells about us</h1>;
+  return (
+    <div>
+      <h1>This page tells about us</h1>
+      <ul>
+        <li>
+          <Link href="/aboutus/1">Yash</Link>
+        </li>
+        <li>
+          <Link href="/aboutus/2">Vaibhav</Link>
+        </li>
+        <li>
+          <Link href="/aboutus/3">Suresh</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default aboutUs;
